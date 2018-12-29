f4791c53b81b817408128b8100fa3084cc64af80
duchenpaul <duchenpaul@gmail.com>
2018-12-29 20:57:44 +0800

    Order profile by key

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ configFile = 'config.ini'
 configDict = toolkit_config.read_config_general(configFile)['PROFILE']
 
 sleep_prof_list = []
-for k, v in configDict.items():
-    sleep_prof_list.append(int(v))
+for key in sorted(configDict):
+    sleep_prof_list.append(configDict[key])
 
 
+print(sleep_prof_list)
