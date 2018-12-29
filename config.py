8847d268df8e59ed6497637480bed3bd3229beae
duchenpaul <duchenpaul@gmail.com>
2018-12-29 20:20:57 +0800

    Fix bug

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import toolkit_config
 SEGMENT = 8
 configFile = 'config.ini'
 
-configDict = toolkit_config.read_config_general(configFile)
+configDict = toolkit_config.read_config_general(configFile)['PROFILE']
 
 sleep_prof_list = []
 for k, v in configDict.items():
